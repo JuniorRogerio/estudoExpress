39a964cb5f55f1f22afab241516a4fc44b64ee7d
JuniorRogerio <rogeriomendesjunior@gmail.com>
2021-04-19 22:43:30 -0300

    create usuarios

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//indica para express que usaremos os dados em json
+app.use(express.json());
+
 let usuarios = [
     {nome: "Rogério", sobrenome: "Mendes"}
 ];
@@ -14,6 +17,21 @@ app.get("/inicio", (request, response) => {
 app.get("/usuarios", (request, response) => {
     //console.log(request)
     return response.json(usuarios);
+})
+
+app.post("/usuarios", (request, response) => {
+    const { nome, sobrenome } = request.body;
+
+    usuarios.push({ nome, sobrenome });
+
+    return response.json({ nome, sobrenome });
+})
+
+//http://localhost:3000/usuarios/Rogério
+app.put("/usuarios/:nome", (request, response) => {
+    const { nome } = request.params;
+
+    const { sobrenome } = request.body;
 })
 
 app.listen(3000, () => {
